5bfe09fc38c067bd4c9590bead0b4d67024d5f46
Gabriel Kieruzel <gabriel.kieruzel@protonmail.com>
2024-03-19 23:09:26 +0100

    Storybook continuation

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { withRouter } from "storybook-addon-remix-react-router";
-
 import { Header } from "../components/header/Header";
 
 const meta: Meta<typeof Header> = {
@@ -21,7 +20,7 @@ export default meta;
 type Story = StoryObj<typeof Header>;
 
 export const Basic: Story = {};
-export const Primary: Story = {
+export const Login: Story = {
   args: {
     heading: "Signup to create account",
     paragraph: "Already have account?",
@@ -33,6 +32,22 @@ export const Primary: Story = {
       description: {
         story: "This is the signup page header component",
       },
+    },
+  },
+};
+
+export const Signup: Story = {
+  args: {
+    ...Login.args,
+    paragraph: "Do not have an account yet?",
+    linkName: "Signup",
+    linkUrl: "/signup",
+  },
+  parameters: {
+    docs: {
+      ...Login.parameters?.docs,
+      linkName: "Signup",
+      linkUrl: "/Signup",
     },
   },
 };
